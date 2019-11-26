85aa774c4641537bcf4e1c5f51ec8f621e82274e
Hitesh <hikus@fotonik.dtu.dk>
2019-11-26 10:33:17 +0100

    Create AlGaAsoS_convergence_study.py

---
+++ runs/AlGaAsoS_convergence_study.py
@@ -0,0 +1,241 @@
+""" Calculate the convergence as a function of FEM mesh for 
+    backward SBS gain spectra of AlGaAsoS
+    hikus
+	Origional: 18 Nov 19
+	Edited: 18 Nov 19
+"""
+
+import time
+import datetime
+import numpy as np
+import sys
+import matplotlib
+matplotlib.use('pdf')
+import matplotlib.pyplot as plt
+
+sys.path.append("../backend/")
+import materials
+import objects
+import mode_calcs
+import integration
+import plotting
+from fortran import NumBAT
+
+
+# Geometric Parameters - all in nm.
+wl_nm = 1550
+unitcell_x = 6*wl_nm
+unitcell_y = unitcell_x
+# Waveguide widths.
+inc_a_x = 700
+inc_a_y = 320
+# Shape of the waveguide.
+inc_shape = 'rib_coated'
+
+slab_a_x = 4000
+slab_a_y = 1000
+
+coat_x = 20 
+coat_y = 20
+
+num_modes_EM_pump = 20
+num_modes_EM_Stokes = num_modes_EM_pump
+num_modes_AC = 50
+EM_ival_pump = 0
+EM_ival_Stokes = 0
+AC_ival = 'All'
+
+prefix_str = 'Al21GaAs_on_Sapp-coated'
+
+# Warning: The fine grids in this list will take considerable time to run!
+lc_list = [20,100,500,1000,1500,2000,2500]
+nu_lcs = len(lc_list)
+lc_bkg_list = 1*np.ones(nu_lcs)
+x_axis = lc_list
+conv_list = []
+time_list = []
+# Do not run in parallel, otherwise there are confusions reading the msh files!
+for i_lc, lc_ref in enumerate(lc_list):
+    start = time.time()
+    print("\n Running simulation", i_lc+1, "/", nu_lcs)
+    lc3 = lc_ref/2
+    lc_bkg = lc_bkg_list[i_lc]
+    wguide = objects.Struct(unitcell_x,inc_a_x,unitcell_y,inc_a_y,inc_shape,
+                        slab_a_x=slab_a_x, slab_a_y=slab_a_y, coat_x=coat_x, coat_y=coat_y,
+                        material_bkg=materials.Vacuum,
+                        material_a=materials.Al21GaAs, # waveguide
+                        material_b=materials.Sapphire,     # slab
+                        material_c=materials.Sapphire,     # coating
+                        lc_bkg=lc_bkg, lc2=lc_ref, lc3=lc3, lc4=lc_ref, force_mesh=True)
+                        
+    
+
+    # Expected effective index of fundamental guided mode.
+    n_eff = wguide.material_a.n-0.1
+    # Calculate Electromagnetic modes.
+    sim_EM_pump = wguide.calc_EM_modes(num_modes_EM_pump, wl_nm, n_eff)
+    sim_EM_Stokes = mode_calcs.bkwd_Stokes_modes(sim_EM_pump)
+    k_AC = np.real(sim_EM_pump.Eig_values[EM_ival_pump] - sim_EM_Stokes.Eig_values[EM_ival_Stokes])
+    # Calculate Acoustic modes.
+    sim_AC = wguide.calc_AC_modes(num_modes_AC, k_AC, EM_sim=sim_EM_pump)
+    # Calculate interaction integrals and SBS gain.
+    SBS_gain, SBS_gain_PE, SBS_gain_MB, linewidth_Hz, Q_factors, alpha = integration.gain_and_qs(
+        sim_EM_pump, sim_EM_Stokes, sim_AC, k_AC,
+        EM_ival_pump=EM_ival_pump, EM_ival_Stokes=EM_ival_Stokes, AC_ival=AC_ival)
+
+    conv_list.append([sim_EM_pump, sim_AC, SBS_gain, SBS_gain_PE, SBS_gain_MB])
+    end = time.time()
+    time_list.append(end - start)
+
+# It is crucial that you preselect modes with significant gain!
+# Otherwise you will observe large relative errors similar to dividing by zero.
+rel_modes = [3,4,8,10]
+# If you do not know the mode numbers of the significant AC modes you may wish to simply plot them all
+# by uncommenting the line below and check if the modes with large gain have low relative errors.
+rel_modes = np.linspace(0,num_modes_AC-1,num_modes_AC)
+rel_mode_freq_EM = np.zeros(nu_lcs,dtype=complex)
+rel_mode_freq_AC = np.zeros((nu_lcs,len(rel_modes)),dtype=complex)
+rel_mode_gain = np.zeros((nu_lcs,len(rel_modes)),dtype=complex)
+rel_mode_gain_MB = np.zeros((nu_lcs,len(rel_modes)),dtype=complex)
+rel_mode_gain_PE = np.zeros((nu_lcs,len(rel_modes)),dtype=complex)
+for i_conv, conv_obj in enumerate(conv_list):
+    rel_mode_freq_EM[i_conv] = conv_obj[0].Eig_values[0]
+    for i_m, rel_mode in enumerate(rel_modes):
+        rel_mode_freq_AC[i_conv,i_m] = conv_obj[1].Eig_values[rel_mode]
+        rel_mode_gain[i_conv,i_m] = conv_obj[2][EM_ival_Stokes,EM_ival_pump,rel_mode]
+        rel_mode_gain_PE[i_conv,i_m] = conv_obj[3][EM_ival_Stokes,EM_ival_pump,rel_mode]
+        rel_mode_gain_MB[i_conv,i_m] = conv_obj[4][EM_ival_Stokes,EM_ival_pump,rel_mode]
+
+
+
+xlabel = "Mesh Refinement Factor"
+fig = plt.figure()
+plt.clf()
+ax1 = fig.add_subplot(1,1,1)
+ax2 = ax1.twinx()
+ax2.yaxis.tick_left()
+ax2.yaxis.set_label_position("left")
+EM_plot_Mk = rel_mode_freq_EM*1e-6
+error0 = np.abs((np.array(EM_plot_Mk[0:-1])-EM_plot_Mk[-1])/EM_plot_Mk[-1])
+ax2.plot(x_axis[0:-1], error0, 'b-v',label='Mode #%i'%EM_ival_pump)
+ax1.plot(x_axis, np.real(EM_plot_Mk), 'r-.o',label=r'EM k$_z$')
+ax1.yaxis.tick_right()
+ax1.spines['right'].set_color('red')
+ax1.yaxis.label.set_color('red')
+ax1.yaxis.set_label_position("right")
+ax1.tick_params(axis='y', colors='red')
+handles, labels = ax2.get_legend_handles_labels()
+ax2.legend(handles, labels)
+ax1.set_xlabel(xlabel)
+ax1.set_ylabel(r"EM k$_z$ ($\times 10^6$ 1/m)")
+ax2.set_ylabel(r"Relative Error EM k$_z$")
+ax2.set_yscale('log')#, nonposx='clip')
+plt.savefig(prefix_str+'convergence-freq_EM.pdf', bbox_inches='tight')
+plt.savefig(prefix_str+'convergence-freq_EM.png', bbox_inches='tight')
+plt.close()
+
+fig = plt.figure()
+plt.clf()
+ax1 = fig.add_subplot(1,1,1)
+ax2 = ax1.twinx()
+ax2.yaxis.tick_left()
+ax2.yaxis.set_label_position("left")
+for i_m, rel_mode in enumerate(rel_modes):
+    rel_mode_freq_AC_plot_GHz = rel_mode_freq_AC[:,i_m]*1e-9
+    error0 = np.abs((np.array(rel_mode_freq_AC_plot_GHz[0:-1])-rel_mode_freq_AC_plot_GHz[-1])/rel_mode_freq_AC_plot_GHz[-1])
+    ax2.plot(x_axis[0:-1], error0, '-v',label='Mode #%i'%rel_mode)
+    ax1.plot(x_axis, np.real(rel_mode_freq_AC_plot_GHz), '-.o',label=r'AC Freq mode #%i'%rel_mode)
+ax1.yaxis.tick_right()
+ax1.spines['right'].set_color('red')
+ax1.yaxis.label.set_color('red')
+ax1.yaxis.set_label_position("right")
+ax1.tick_params(axis='y', colors='red')
+handles, labels = ax2.get_legend_handles_labels()
+ax2.legend(handles, labels)
+ax1.set_xlabel(xlabel)
+ax1.set_ylabel(r"AC Freq (GHz)")
+ax2.set_ylabel(r"Relative Error AC Freq")
+ax2.set_yscale('log')#, nonposx='clip')
+plt.savefig(prefix_str+'convergence-freq_AC.pdf', bbox_inches='tight')
+plt.savefig(prefix_str+'convergence-freq_AC.png', bbox_inches='tight')
+plt.close()
+
+fig = plt.figure()
+plt.clf()
+ax1 = fig.add_subplot(1,1,1)
+ax2 = ax1.twinx()
+ax2.yaxis.tick_left()
+ax2.yaxis.set_label_position("left")
+for i_m, rel_mode in enumerate(rel_modes):
+    rel_mode_gain_plot = rel_mode_gain[:,i_m]
+    error0 = np.abs((np.array(rel_mode_gain_plot[0:-1])-rel_mode_gain_plot[-1])/rel_mode_gain_plot[-1])
+    ax2.plot(x_axis[0:-1], error0, '-v',label=r'Mode #%i'%rel_mode)
+    ax1.plot(x_axis, np.real(rel_mode_gain_plot), '-.o',label=r'Gain mode #%i'%rel_mode)
+ax1.yaxis.tick_right()
+ax1.spines['right'].set_color('red')
+ax1.yaxis.label.set_color('red')
+ax1.yaxis.set_label_position("right")
+ax1.tick_params(axis='y', colors='red')
+handles, labels = ax2.get_legend_handles_labels()
+ax2.legend(handles, labels)
+ax1.set_xlabel(xlabel)
+ax1.set_ylabel(r"Gain")
+ax2.set_ylabel(r"Relative Error Gain")
+ax2.set_yscale('log')#, nonposx='clip')
+plt.savefig(prefix_str+'convergence-Gain.pdf', bbox_inches='tight')
+plt.savefig(prefix_str+'convergence-Gain.png', bbox_inches='tight')
+plt.close()
+
+fig = plt.figure()
+plt.clf()
+ax1 = fig.add_subplot(1,1,1)
+ax2 = ax1.twinx()
+ax2.yaxis.tick_left()
+ax2.yaxis.set_label_position("left")
+for i_m, rel_mode in enumerate(rel_modes):
+    rel_mode_gain_PE_plot = rel_mode_gain_PE[:,i_m]
+    error0 = np.abs((np.array(rel_mode_gain_PE_plot[0:-1])-rel_mode_gain_PE_plot[-1])/rel_mode_gain_PE_plot[-1])
+    ax2.plot(x_axis[0:-1], error0, '-v',label=r'Mode #%i'%rel_mode)
+    ax1.plot(x_axis, np.real(rel_mode_gain_PE_plot), '-.o',label=r'Gain mode #%i'%rel_mode)
+ax1.yaxis.tick_right()
+ax1.spines['right'].set_color('red')
+ax1.yaxis.label.set_color('red')
+ax1.yaxis.set_label_position("right")
+ax1.tick_params(axis='y', colors='red')
+handles, labels = ax2.get_legend_handles_labels()
+ax2.legend(handles, labels)
+ax1.set_xlabel(xlabel)
+ax1.set_ylabel(r"Gain (PE)")
+ax2.set_ylabel(r"Relative Error Gain (PE)")
+ax2.set_yscale('log')#, nonposx='clip')
+plt.savefig(prefix_str+'convergence-Gain_PE.pdf', bbox_inches='tight')
+plt.savefig(prefix_str+'convergence-Gain_PE.png', bbox_inches='tight')
+plt.close()
+
+fig = plt.figure()
+plt.clf()
+ax1 = fig.add_subplot(1,1,1)
+ax2 = ax1.twinx()
+ax2.yaxis.tick_left()
+ax2.yaxis.set_label_position("left")
+for i_m, rel_mode in enumerate(rel_modes):
+    rel_mode_gain_MB_plot = rel_mode_gain_MB[:,i_m]
+    error0 = np.abs((np.array(rel_mode_gain_MB_plot[0:-1])-rel_mode_gain_MB_plot[-1])/rel_mode_gain_MB_plot[-1])
+    ax2.plot(x_axis[0:-1], error0, '-v',label=r'Mode #%i'%rel_mode)
+    ax1.plot(x_axis, np.real(rel_mode_gain_MB_plot), '-.o',label=r'Gain mode #%i'%rel_mode)
+ax1.yaxis.tick_right()
+ax1.spines['right'].set_color('red')
+ax1.yaxis.label.set_color('red')
+ax1.yaxis.set_label_position("right")
+ax1.tick_params(axis='y', colors='red')
+handles, labels = ax2.get_legend_handles_labels()
+ax2.legend(handles, labels)
+ax1.set_xlabel(xlabel)
+ax1.set_ylabel(r"Gain (MB)")
+ax2.set_ylabel(r"Relative Error Gain (MB)")
+ax2.set_yscale('log')#, nonposx='clip')
+plt.savefig(prefix_str+'convergence-Gain_MB.pdf', bbox_inches='tight')
+plt.savefig(prefix_str+'convergence-Gain_MB.png', bbox_inches='tight')
+plt.close()
+
+print("Calculation time", time_list)
